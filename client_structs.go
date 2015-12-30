30c0200fb35e73377c62888fafb6702bac1df343
Kevin Kirsche <kev.kirsche@gmail.com>
2015-12-30 12:59:18 -0500

    Add godocs for the client struct

--- client_structs.go
+++ client_structs.go
@@ -1,5 +1,8 @@
+// Package goNessus provides a Golang based interface to Nessus 6
 package goNessus
 
+// Nessus struct is used to contain information about a Nessus scanner. This
+// will be used to connect to the scanner and make API requests.
 type Nessus struct {
 	Ip        string
 	Port      string
