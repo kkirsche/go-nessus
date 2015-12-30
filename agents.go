ca5bf1cb00582e77675ecbb3372a91cc96233434
Kevin Kirsche <kev.kirsche@gmail.com>
2015-12-30 12:57:35 -0500

    GoDocs

--- agents.go
+++ agents.go
@@ -1,9 +1,14 @@
+// Package goNessus provides a Golang based interface to Nessus 6
 package goNessus
 
 import (
 	"fmt"
 )
 
+// Returns the agent list for the given scanner.
+//
+// This request requires standard user permissions. Users with this role can
+// create scans, policies, and reports.
 func (nessus *Nessus) ListAgents(scanner_id int, str_ch chan string) {
 	agents_ch := make(chan string)
 	url := fmt.Sprintf("scanners/%d/agents", scanner_id)
